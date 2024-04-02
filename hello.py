2b508cb07cc2f406b1fcfaf8538d3e83bbb7012b
Poctar <127279445+Poctar@users.noreply.github.com>
2024-04-02 17:27:47 +0900

    파이썬 0402

---
+++ hello.py
@@ -0,0 +1,70 @@
+#  print("Hello, world")
+# x =1
+# y =2
+# z = x+y-x
+# print(z)
+
+# apple = "a"  # "a" = 문자
+# banana = "b"  #  "b" = 문자
+
+# print("hello, world!") #매개변수
+# print() # 함수, 인수
+
+# cmd 명령어 clear를 입력하면 cmd창 기록 지워줌
+# 화살표 위 아래 버튼으로 cmd에 입력했던 기존 명령어 빠르게 찾을수 있음
+
+# name = input("What's your name? ") # crtl + z 되돌리기!
+# print("너의 이름은",name)
+
+# print("hello, ", "name", "name2", sep ='') # 매개변수간 공백 제거후 출력
+# print("hello, ", "name", "name2") # 기본 문장 출력
+# print("hello, ", "name", "name2", end ='') # 개행 없이(enter 없이) 다음 명령어 입력
+
+# name = input("What's your name? ")
+# print("hello, ", end='') # "hello, " 아닌 'hello ' 해도 작동됨  ' ', " "를 쓰지 않게 되면 변수로 인식 ,  (' ' ," " 안에 있는거는 string 으로 인식)
+# print(name) # " ", ' ' 는 위에 정의가 되어 있기 떄문에 적용 안해도 괜찮다!
+
+
+# name = input("What's your name? ")
+# print('my "name" is' , end='') # 이 방법 말고 ' ', " " 를 바뀌서 해도 출력이 된다
+# print(name)
+
+# name = input("What's your name? ")
+# print("my 'name' is" , end='') # 이 방법 말고 ' ', " " 를 바뀌서 해도 출력이 된다
+# print(name)
+
+# name = input("What's your name? ")
+# print(f"Hello, {name}")
+
+# name = input("What's your name? ")
+
+# name = name.strip() # 앞에 쳐있는 공백을 제거하고 출력한다.
+
+# print(f"Hello, {name}")
+
+
+# name = input("What's your name? ")
+
+# # name = name.title() # 첫 단어 대문자로 표현 공백을 제거 x
+
+# # #name = name.strip() # 앞에 쳐있는 공백을 제거하고 출력한다.
+
+# name = name.strip().capitalize() # 첫 단어 대문자로 표현 공백을 제거 name = name.strip().title()로도 가능!
+
+# print(f"Hello, {name}")
+
+# def hello():  # 함수의 시작 정의는 def
+#     print("Hello, ") # hello 라는 함수를 먼저 Hello로 나오도록 정의
+
+# name = input("What's your name? ")
+# hello()
+# print(name)
+
+# def hello(x,e):
+#     print("Hello,", x, e) #3
+
+# name = input("What's your name? ") #1
+# age = input("What's your age? ") #1
+# d = input("ㅋㅋ?")
+# hello(name,d) #2
+
